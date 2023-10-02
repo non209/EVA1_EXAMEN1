188ed7ac1c883e95e70d691b49ab66b9f37d3f44
adrxnrdgz <143474113+adrxnrdgz@users.noreply.github.com>
2023-10-01 20:16:10 -0700

    Add files via upload

---
+++ EJER1/src/ejer1_eq/EJER1_EQ.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejer1_eq;
+
+import javax.swing.GroupLayout.Group;
+
+/**
+ *
+ * @author angel
+ */
+public class EJER1_EQ {
+
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+              
+     String equipo = "Java Group";
+     String proyecto = "StockCheck";
+     
+     String nombre1 = "Hernández Granados,Jorge Alejandro, Num. Ctrl: 23550419";
+     String nombre2 = "Ponce Banda. Ángel Yahir, Num. Ctr: 23550330";
+     String nombre3 = "Orozco Rodríguez. Ángel Adrián, Num Ctr: 23550328";
+     String nombre4 = "Gónzalez Ávila, Fernando, Num. Ctr: 23550348";
+     
+    System.out.println("Nombre del equipo: " + equipo);
+    System.out.println("Nombre del proyecto: " + proyecto);
+    System.out.println("Integrantes del equipo: " + nombre1);
+    System.out.println(nombre2);
+    System.out.println(nombre3);
+    System.out.println(nombre4);
+    
+    
+    
+        
+    }
+
+   
+    
+}
